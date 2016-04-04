eeff70cfe9d7470cc454071e3416e4e1fbc71def
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2016-04-04 21:56:58 +0200

    added FxKeyNodeFileStore

---
+++ src/main/java/fr/an/fxtree/impl/helper/FxKeyNodeFileStore.java
@@ -0,0 +1,139 @@
+package fr.an.fxtree.impl.helper;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.an.fxtree.format.FxFileUtils;
+import fr.an.fxtree.impl.model.mem.FxMemRootDocument;
+import fr.an.fxtree.model.FxChildWriter;
+import fr.an.fxtree.model.FxNode;
+import fr.an.fxtree.model.FxObjNode;
+
+/**
+ * helper class to store node by (flat) keys in file
+ * 
+ *  thread-safety: thread-safe, protected by <code>lock</code>
+ */
+public class FxKeyNodeFileStore {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(FxKeyNodeFileStore.class);
+    
+    private Object lock = new Object();
+    
+    private File storeFile;
+    private FxObjNode contentObj;
+    
+    // ------------------------------------------------------------------------
+
+    public FxKeyNodeFileStore(File storeFile) {
+        this.storeFile = storeFile;
+        if (storeFile.exists()) {
+            this.contentObj = (FxObjNode) FxFileUtils.readTree(storeFile);
+        } else {
+            FxMemRootDocument doc = new FxMemRootDocument();
+            this.contentObj = doc.setContentObj();
+            try {
+                FxFileUtils.writeTree(storeFile, contentObj);
+            } catch(Exception ex) {
+                throw new RuntimeException("can not write to file '" + storeFile + "'", ex);
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    public File getStoreFile() {
+        return storeFile;
+    }
+
+    public Set<String> keySet() {
+        synchronized (lock) {
+            Set<String> res = new LinkedHashSet<>();
+            for (Iterator<Entry<String, FxNode>> iter = contentObj.fields(); iter.hasNext(); ) {
+                res.add(iter.next().getKey());
+            }
+            return res;
+        }
+    }
+
+    public boolean containsKey(String key) {
+        synchronized (lock) {
+            FxNode tmpres = contentObj.get(key);
+            return (tmpres != null);
+        }
+    }
+
+    /** unsafe .. should not modify node from outside !*/
+    public FxNode _unsafeGet(String key) {
+        synchronized (lock) {
+            return contentObj.get(key);
+        }
+    }
+    
+    public FxNode getCopy(String key) {
+        synchronized (lock) {
+            FxNode tmpres = contentObj.get(key);
+            return (tmpres != null)? FxNodeCopyVisitor.cloneMemNode(tmpres) : null;
+        }
+    }
+
+    public void getCopyTo(FxChildWriter out, String key) {
+        synchronized (lock) {
+            FxNode tmpres = contentObj.get(key);
+            if (tmpres == null) {
+                // add null or empty/nothing?
+                return;
+            } else {
+                FxNodeCopyVisitor.copyTo(out, tmpres);
+            }
+        }
+    }
+
+    /**
+     * @param key
+     * @param nodeToCopy node to copy for key
+     */
+    public void put(String key, FxNode nodeToCopy) {
+        synchronized (lock) {
+            // FxNode prev = contentObj.get(key);
+            FxChildWriter writer = contentObj.putBuilder(key);
+            FxNodeCopyVisitor.copyTo(writer, nodeToCopy);
+            flushWrite(key);
+        }
+    }
+
+    /**
+     * @param key
+     */
+    public void remove(String key) {
+        synchronized (lock) {
+            FxNode prev = contentObj.remove(key);
+            if (prev != null) {
+                flushWrite(key);
+            }
+        }
+    }
+
+    protected void flushWrite(String key) {
+        try {
+            FxFileUtils.writeTree(storeFile, contentObj);
+        } catch(Exception ex) {
+            LOG.warn("Failed to write keyNodeStore to file '" + storeFile + "' (value for key '" + key + "' may be lost on stop/restart) ... ignore, no rethrow", ex);
+        }        
+    }
+
+    // ------------------------------------------------------------------------
+    
+    @Override
+    public String toString() {
+        return "FxKeyNodeFileStore [storeFile=" + storeFile + "]";
+    }
+
+    
+}
---
+++ src/test/java/fr/an/fxtree/impl/helper/FxKeyNodeFileStoreTest.java
@@ -0,0 +1,73 @@
+package fr.an.fxtree.impl.helper;
+
+import java.io.File;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
+
+import fr.an.fxtree.impl.model.mem.FxMemRootDocument;
+import fr.an.fxtree.impl.util.FxNodeAssert;
+import fr.an.fxtree.model.FxIntNode;
+import fr.an.fxtree.model.FxNode;
+import fr.an.fxtree.model.FxObjNode;
+
+public class FxKeyNodeFileStoreTest {
+
+    protected FxKeyNodeFileStore sut;
+    
+    @Before 
+    public void setup() {
+        File dir = new File("target/test");
+        if (! dir.exists()) {
+            dir.mkdirs();
+        }
+        File storeFile = new File(dir, "test-keyNodeFileStore.yaml");
+        sut = new FxKeyNodeFileStore(storeFile);
+    }
+    
+    @Test
+    public void testPut_get_remove_reload() {
+        FxObjNode objNode = new FxMemRootDocument().setContentObj();
+        FxIntNode value123 = objNode.put("value123", 123);
+        sut.put("key1", value123);
+        
+        FxObjNode obj1 = objNode.putObj("obj1");
+        obj1.put("field1", "value1");
+        sut.put("key2", obj1);
+
+        FxNode obj1Ref = sut._unsafeGet("key2");
+        Assert.assertNotNull(obj1Ref);
+        Assert.assertNotSame(obj1, obj1Ref);
+        
+        Set<String> ks = sut.keySet();
+        Assert.assertEquals(ImmutableSet.of("key1", "key2"), ks);
+        
+        Assert.assertTrue(sut.containsKey("key1"));
+        Assert.assertFalse(sut.containsKey("unknnown-key"));
+        
+        FxNode value123Copy = sut.getCopy("key1");
+        FxNodeAssert.assertEquals(value123, value123Copy);
+
+        FxNode obj1Copy = sut.getCopy("key2");
+        FxNodeAssert.assertEquals(obj1, obj1Copy);
+
+        FxMemRootDocument tmpDoc = new FxMemRootDocument();
+        sut.getCopyTo(tmpDoc.contentWriter(), "key2");
+        FxNode obj1Copy2 = tmpDoc.getContent();
+        FxNodeAssert.assertEquals(obj1, obj1Copy2);
+        
+        sut.remove("key1");
+        FxNode value123Copy_null = sut.getCopy("key1");
+        Assert.assertNull(value123Copy_null);
+        
+        FxKeyNodeFileStore sutReload = new FxKeyNodeFileStore(sut.getStoreFile());
+        FxNodeAssert.assertEquals(obj1, sutReload.getCopy("key2"));
+        
+        Assert.assertNotNull(sut.toString());
+    }
+    
+}
